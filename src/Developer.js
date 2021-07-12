57a2b1d43d88d4e4e3cb2fee3f69c3329e7d4dfd
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2021-07-12 13:22:19 -0700

    dev and user components

---
+++ src/Developer.js
@@ -0,0 +1,11 @@
+import './Developer.css'
+
+function Developer() {
+  return(
+    <div className="Developer">
+      <h1>developer</h1>
+    </div>
+  )
+}
+
+export default Developer;
---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import './Home.css';
+
+
+function Home() {
+  return (
+    <div className="Home">
+      <h1>conversational app builder</h1>
+      <button>Developer
+      </button>
+      <button>User
+      </button>
+    </div>
+  )
+}
+
+export default Home;
