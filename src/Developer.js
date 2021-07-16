e918d109c11b060a285d5c36670610e91148100b
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2021-07-16 13:09:01 -0700

    file uploads - with image dev

--- src/Developer.js
+++ src/Developer.js
@@ -1,23 +1,74 @@
 import './Developer.css'
+import axios from 'axios';
+import React,{Component} from 'react';
 import { NavLink } from 'react-router-dom' 
 
-function Developer() {
-  return(
-    <div className="Developer">
-      <h1>developer</h1>
-      <NavLink to="/">
-        <button class="home">
-        Home        
-        </button>
-      </NavLink>
-      <div className="uploadFile"> 
-        <input type="file"/> 
-        <button class="upload"> 
-          Upload! 
-        </button> 
-      </div>  
-    </div>
-  )
-}
+class Developer extends Component {
+    state = {
+      selectedFile: null,
+    };
+    
+    onFileChange = event => {
+      this.setState({ selectedFile: event.target.files[0] });
+    };
 
+    onFileUpload = () => {
+      const formData = new FormData();
+      formData.append(
+        "myFile",
+        this.state.selectedFile,
+        this.state.selectedFile.name, 
+        this.state.selectedFile.size, 
+        this.state.selectedFile.ImageThumb
+      );
+      console.log(this.state.selectedFile);
+      axios.post("api/uploadfile", formData);
+    };
+    
+    fileData = () => {
+      if (this.state.selectedFile) {
+        return (
+          <div>
+            <h2>File Details:</h2>
+            <p>File Name: {this.state.selectedFile.name}</p>
+            <p>File Type: {this.state.selectedFile.type}</p>
+            <p>File Size: {this.state.selectedFile.size}</p>
+            <p>File Img: {this.state.selectedFile.ImageThumb}</p>
+          </div>
+        );
+      } else {
+        return (
+          <div>
+            <h1>select a file</h1>
+          </div>
+        );
+      }
+    };
+
+    render() {
+      return (
+        <div>
+          <h1>developer</h1>
+          <NavLink to="/">
+          <button class="home">
+            Home        
+          </button>
+          </NavLink>
+          <div>
+            <input type="file" onChange={this.onFileChange} />
+            <button onClick={this.onFileUpload}>
+              Upload!
+            </button>
+          </div>
+          {this.fileData()}
+          {this.state.selectedFile && <ImageThumb image={this.state.selectedFile} />}
+        </div>
+      );
+    }
+  }
+
+  const ImageThumb = ({ image }) => {
+    return <img src={URL.createObjectURL(image)} alt={image.name} />;
+  };
+  
 export default Developer;
