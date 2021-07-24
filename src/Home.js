6c5930ff0f546fb88323a449ef4619bc24e3dfa6
Nandan01 <36855411+Nandan01@users.noreply.github.com>
2021-07-24 10:08:14 -0400

    react developer

--- src/Home.js
+++ src/Home.js
@@ -5,15 +5,25 @@ import { NavLink } from 'react-router-dom'
 function Home() {
   return (
     <div className="Home">
-      <h1>conversational app builder</h1>
-      <NavLink to="/developer">
-        <button class="developer">
+      <h1>ConvaBuild</h1>
+      <NavLink to="/Home">
+        <button class="Home">
+          Home        
+        </button>
+      </NavLink> 
+      <NavLink to="/About">
+        <button class="About">
+          About         
+        </button>
+      </NavLink> 
+      <NavLink to="/Developer">
+        <button class="Developer">
           Developer        
         </button>
       </NavLink> 
-      <NavLink to="/user">
-        <button class="user">
-          User        
+      <NavLink to="/User">
+        <button class="User">
+          User       
         </button>
       </NavLink> 
     </div>
