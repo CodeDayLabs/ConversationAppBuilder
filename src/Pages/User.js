b1c932dd452c797c43e6e88c2e3b09a0cd335872
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2021-08-03 18:02:00 -0700

    table

--- src/Pages/User.js
+++ src/Pages/User.js
@@ -1,25 +1,11 @@
 import React from 'react';
 import './User.css';
-
+import DataTable from './DataTable';
 
 function User() {
   return (
     <div className="User">
-      <div className="categories">
-        <button>Category</button> 
-        <button>Category</button> 
-        <button>Category</button> 
-        <button>Category</button> 
-        <button>Category</button> 
-      </div>
-      <div className="options">
-        <p>Name of Program</p>
-        <button>Select</button> 
-      </div> 
-      <input></input>
-      <div className="output">
-        Ouput to question.
-      </div>
+      <DataTable />
     </div>
   )
 }
